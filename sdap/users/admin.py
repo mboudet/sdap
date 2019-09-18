fa8c738c0829215f14969da6ddedf46c2bd6d68d
mboudet <mateo.boudet@irisa.fr>
2019-09-18 08:34:38 +0000

    fix

--- sdap/users/admin.py
+++ sdap/users/admin.py
@@ -13,7 +13,7 @@ class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": [("name",), ("last_name",), ("institut",),]}),) + auth_admin.UserAdmin.fieldsets
+    fieldsets = (("User", {"fields": [("name",)]}),) + auth_admin.UserAdmin.fieldsets
     list_display = ["username", "name", "last_name", "institut", "is_superuser"]
     search_fields = ["name"]
 
