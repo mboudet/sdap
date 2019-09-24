83b5347bc0ce479e32357c45267a436bcb0c5dcf
mboudet <mateo.boudet@irisa.fr>
2019-09-24 08:39:48 +0000

    File and folder creation

--- sdap/files/forms.py
+++ sdap/files/forms.py
@@ -5,6 +5,8 @@ from crispy_forms.layout import Submit, Layout
 from crispy_forms.bootstrap import FormActions
 
 from django.core.validators import MaxValueValidator, MinValueValidator
+from .models import File, Folder
+
 
 class VisualisationArgumentForm(forms.Form):
 
@@ -23,3 +25,60 @@ class VisualisationArgumentForm(forms.Form):
         self.helper = FormHelper(self)
         self.helper.form_id= "visualization_form"
         self.helper.form_method = 'POST'
+
+class FileCreateForm(forms.ModelForm):
+
+    folder = forms.ModelChoiceField(Folder.objects.all(), empty_label="/")
+
+    class Meta:
+        model = File
+        fields = ["name", "description", "type", "file", "folder"]
+        labels = {
+            "name": "Name of the file",
+            "description": "A short description of the file",
+            "type": "Type of the file",
+            "folder": "This file will be placed in this folder",
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.current_folder = kwargs.pop('current_folder', None)
+        self.folders = kwargs.pop('folders', None)
+
+        super(FileCreateForm, self).__init__(*args, **kwargs)
+
+        if self.folders:
+            self.fields['folder'].queryset = self.folders
+            if self.current_folder:
+                self.fields['folder'].initial = self.current_folder
+
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'POST'
+        self.helper.add_input(Submit('save', 'Save'))
+
+class FolderCreateForm(forms.ModelForm):
+
+    folder = forms.ModelChoiceField(Folder.objects.all(), empty_label="/")
+
+    class Meta:
+        model = Folder
+        fields = ["name", "folder"]
+        labels = {
+            "name": "Name of the file",
+            "folder": "Containing folder",
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.current_folder = kwargs.pop('current_folder', None)
+        self.folders = kwargs.pop('folders', None)
+
+        super(FolderCreateForm, self).__init__(*args, **kwargs)
+
+        if self.folders:
+            self.fields['folder'].queryset = self.folders
+            if self.current_folder:
+                self.fields['folder'].initial = self.current_folder
+
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'POST'
+        self.helper.add_input(Submit('save', 'Save'))
+
---
+++ sdap/static/js/file.js
@@ -0,0 +1,75 @@
+$(function () {
+
+  /* Functions */
+
+  var loadForm = function () {
+    var btn = $(this);
+    $.ajax({
+      url: btn.attr("data-url"),
+      type: 'get',
+      dataType: 'json',
+      beforeSend: function () {
+        $("#modal-group").modal("show");
+      },
+      success: function (data) {
+        $("#modal-group .modal-content").html(data.html_form);
+      }
+    });
+  };
+
+  var saveForm = function () {
+    var form = $(this);
+    var data = form.serialize();
+    $.ajax({
+      url: form.attr("action"),
+      data: data,
+      type: form.attr("method"),
+      dataType: 'json',
+      success: function (data) {
+        if (data.form_is_valid) {
+            window.location.href = data.redirect
+        }
+        else {
+          $("#modal-group .modal-content").html(data.html_form);
+          if (data.error){
+            $("#modal-group .modal-content #error").html(data.error);
+          }
+        }
+      }
+    });
+    return false;
+  };
+
+  var saveFormFile = function (e) {
+    e.preventDefault();
+    var form = $(this);
+    //var data = form.serialize();
+    var data = new FormData(this)
+    console.log(data);
+    $.ajax({
+      url: form.attr("action"),
+      data: data,
+      type: form.attr("method"),
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+      success: function (return_data) {
+        if (return_data.form_is_valid) {
+            window.location.href = return_data.redirect
+        }
+        else {
+          $("#modal-group .modal-content").html(return_data.html_form);
+          if (return_data.error){
+            $("#modal-group .modal-content #error").html(return_data.error);
+          }
+        }
+      }
+    });
+    return false;
+  };
+
+  /* Binding */
+    $(".js-create").on("click", loadForm);
+    $("#modal-group").on("submit", ".js-create", saveForm);
+    $("#modal-group").on("submit", ".js-create-file", saveFormFile);
+});
