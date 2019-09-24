d2a107955acf54e740f3b08d0766fc7352676ce8
mboudet <mateo.boudet@irisa.fr>
2019-09-24 12:48:40 +0000

    Bug fix + delete function for files & folders

--- sdap/files/models.py
+++ sdap/files/models.py
@@ -1,9 +1,12 @@
 import datetime
+import os
+
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import  User
 from django.conf import settings
 
+from django.dispatch import receiver
 
 # Create your models here.
 class Folder(models.Model):
@@ -43,3 +46,25 @@ class File(models.Model):
     def __str__(self):
         return self.name
 
+
+@receiver(models.signals.post_delete, sender=File)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+
+# Delete in case of update
+@receiver(models.signals.pre_save, sender=File)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = File.objects.get(pk=instance.pk).file
+    except File.DoesNotExist:
+        return False
+
+    new_file = instance.file
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
